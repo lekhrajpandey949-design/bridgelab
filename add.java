a2d806cdc615e0d36f3eb821fa972e0b54a85bed
lekhrajpandey949-design <lekhrajpandey949@gmail.com>
2025-08-06 11:18:25 +0530

    new commit

---
+++ add.java
@@ -0,0 +1,10 @@
+public class add {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        int sum = num1 + num2;  // adding the numbers
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
